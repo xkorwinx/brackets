0472c6b48908d8d85ad2a84973733f1ec793a9f7
Mikalai Zhohun <xkorwinx@gmail.com>
2022-07-20 20:04:51 +0300

    feat: add brackets solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+let obj = {};
+let objRepeat = {};
+for (let j = 0; j < bracketsConfig.length; j++) {
+  if ( bracketsConfig[j][1]===bracketsConfig[j][0]) {
+    objRepeat[bracketsConfig[j][1]] = bracketsConfig[j][0]
+  }
+  else obj[bracketsConfig[j][1]] = bracketsConfig[j][0]
+}
+//console.log(obj)
+let openBr = Object.values(obj);
+let closeBr = Object.keys(obj);
+let repeatBr = Object.values(objRepeat);
+let steck = [];
+//if ((openBr.includes(str[str.length-1]))|| (closeBr.includes(str[0]))) return false;
+for (let i = 0; i < str.length; i++) {
+  if (repeatBr.includes(str[i])) {(steck[steck.length-1] === str[i]) ? steck.pop() : steck.push(str[i]);}
+  else if (openBr.includes(str[i])) { steck.push(str[i])}
+  else if ( ((steck[steck.length-1]) === (obj[str[i]])) ) {
+    steck.pop();}
+  else {return false}
+  }
+if (steck.length) {return false}
+else { return true}
 }
